d4cb3106116ca155d4ce20470bdbbf12e67cd772
ermurd <ermurd@ttu.ee>
2024-10-03 14:47:28 +0300

    Renamed MapTypeController to BeatmapTypeController

--- backend/src/main/java/com/tourneyhub/backend/controller/BeatmapTypeController.java
+++ backend/src/main/java/com/tourneyhub/backend/controller/BeatmapTypeController.java
@@ -1,6 +1,6 @@
 package com.tourneyhub.backend.controller;
 
-import com.tourneyhub.backend.dto.MapTypeDto;
+import com.tourneyhub.backend.dto.BeatmapTypeDto;
 import com.tourneyhub.backend.service.MapTypeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class MapTypeController {
+public class BeatmapTypeController {
 
     private final MapTypeService service;
 
-    public MapTypeController(MapTypeService service) {
+    public BeatmapTypeController(MapTypeService service) {
         this.service = service;
     }
 
     @GetMapping("/api/mapTypes")
-    public List<MapTypeDto> getAll() {
+    public List<BeatmapTypeDto> getAll() {
         return service.getAll();
     }
 }
