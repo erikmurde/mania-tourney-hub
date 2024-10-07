cab068427b11f9c7403e158a925981d5b03dc482
Ermurd <ermurd@taltech.ee>
2024-10-07 20:24:47 +0300

    Edited success dialog UI

--- tourneyhub/src/components/tournament/dialog/SuccessDialog.tsx
+++ tourneyhub/src/components/tournament/dialog/SuccessDialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogTitle, Button, Typography } from '@mui/material';
+import { Dialog, DialogTitle, Button, Typography, Divider } from '@mui/material';
 import { StyledDialogActions } from '../../styled/StyledDialogActions';
 import { Check } from '@mui/icons-material';
 
@@ -19,12 +19,13 @@ const SuccessDialog = ({title, open, setOpen, onClose}: IProps) => {
     }
 
     return (  
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle sx={{ paddingBottom: 0 }}>
-                <Typography textAlign='center' marginBottom={1}>
+        <Dialog open={open} onClose={handleClose} maxWidth='sm' fullWidth>
+            <DialogTitle sx={{ paddingBottom: 0 }} textAlign='center'>
+                <Typography marginBottom={1}>
                     <Check color='success' sx={{ fontSize: 80 }}/>
                 </Typography>
                 {title}
+                <Divider sx={{ marginTop: 1 }}/>
             </DialogTitle>
             <StyledDialogActions sx={{ justifyContent: 'center' }}>
                 <Button variant='contained' onClick={handleClose}>
